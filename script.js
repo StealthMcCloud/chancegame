4485c8f5d6c53ce2f0e5d5e80710507e5db30b06
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-15 13:39:38 -0400

    Finished RPSLS

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let option = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+const option = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+let scoreCounterHuman = 0;
+let scoreCounterComputer = 0;
 
 function rock() {
     let result = option[Math.floor(Math.random() * option.length)];
-    document.getElementById("result1").textContent = result;
+    document.getElementById("result1").textContent = ("Computer chooses " + result + "!");
+    document.getElementById("humanScore").textContent = ("Human: " + scoreCounterHuman);
+    document.getElementById("computerScore").textContent = ("Computer: " + scoreCounterComputer);
 
     if(result == option[1]) {
         document.getElementById("outcome").innerHTML = ("You have been smothered!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[0]) {
@@ -14,26 +19,33 @@ function rock() {
 
     if(result == option[2]) {
         document.getElementById("outcome").innerHTML = ("You smashed the scissors into pieces!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[3]) {
         document.getElementById("outcome").innerHTML = ("You crushed the lizard!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[4]) {
         document.getElementById("outcome").innerHTML = ("You have been vaporized by Spock!(Lose)");
+        scoreCounterComputer += 1;
     }
 
-
+    return scoreCounterComputer;
+    return scoreCounterHuman;
     return result;
 }
 
 function paper() {
     let result = option[Math.floor(Math.random() * option.length)];
-    document.getElementById("result1").textContent = result;
+    document.getElementById("result1").textContent = ("Computer chooses " + result + "!");
+    document.getElementById("humanScore").textContent = ("Human: " + scoreCounterHuman);
+    document.getElementById("computerScore").textContent = ("Computer: " + scoreCounterComputer);
 
     if(result == option[2]) {
         document.getElementById("outcome").innerHTML = ("You have been cut to pieces!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[1]) {
@@ -42,25 +54,33 @@ function paper() {
 
     if(result == option[0]) {
         document.getElementById("outcome").innerHTML = ("You smothered the rock!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[3]) {
         document.getElementById("outcome").innerHTML = ("You have been eaten by the lizard!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[4]) {
         document.getElementById("outcome").innerHTML = ("You have disproved Spock!(Win)");
+        scoreCounterHuman += 1;
     }
 
+    return scoreCounterComputer;
+    return scoreCounterHuman;
     return result;
 }
 
 function scissors() {
     let result = option[Math.floor(Math.random() * option.length)];
-    document.getElementById("result1").textContent = result;
+    document.getElementById("result1").textContent = ("Computer chooses " + result + "!");
+    document.getElementById("humanScore").textContent = ("Human: " + scoreCounterHuman);
+    document.getElementById("computerScore").textContent = ("Computer: " + scoreCounterComputer);
 
     if(result == option[0]) {
         document.getElementById("outcome").innerHTML = ("You have been crushed to pieces!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[2]) {
@@ -69,32 +89,42 @@ function scissors() {
 
     if(result == option[1]) {
         document.getElementById("outcome").innerHTML = ("You cut the paper into pieces!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[3]) {
         document.getElementById("outcome").innerHTML = ("You have decapitated the lizard!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[4]) {
         document.getElementById("outcome").innerHTML = ("You have been smashed by Spock(Lose)");
+        scoreCounterComputer += 1;
     }
 
+    return scoreCounterComputer;
+    return scoreCounterHuman;
     return result;
 }
 function lizard() {
     let result = option[Math.floor(Math.random() * option.length)];
-    document.getElementById("result1").textContent = result;
+    document.getElementById("result1").textContent = ("Computer chooses " + result + "!");
+    document.getElementById("humanScore").textContent = ("Human: " + scoreCounterHuman);
+    document.getElementById("computerScore").textContent = ("Computer: " + scoreCounterComputer);
 
     if(result == option[0]) {
         document.getElementById("outcome").innerHTML = ("You have been crushed!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[2]) {
         document.getElementById("outcome").innerHTML = ("You have been decapitated!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[1]) {
         document.getElementById("outcome").innerHTML = ("You have a good lunch. Yum Yum!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[3]) {
@@ -103,64 +133,44 @@ function lizard() {
 
     if(result == option[4]) {
         document.getElementById("outcome").innerHTML = ("You have poisoned Spock(Win)");
+        scoreCounterHuman += 1;
     }
 
+    return scoreCounterComputer;
+    return scoreCounterHuman;
     return result;
 }
 function spock() {
     let result = option[Math.floor(Math.random() * option.length)];
-    document.getElementById("result1").textContent = result;
+    document.getElementById("result1").textContent = ("Computer chooses " + result) + "!";
+    document.getElementById("humanScore").textContent = ("Human: " + scoreCounterHuman);
+    document.getElementById("computerScore").textContent = ("Computer: " + scoreCounterComputer);
 
     if(result == option[0]) {
         document.getElementById("outcome").innerHTML = ("You have vaporized the rock!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[2]) {
         document.getElementById("outcome").innerHTML = ("You have smashed the scissors to pieces!(Win)");
+        scoreCounterHuman += 1;
     }
 
     if(result == option[1]) {
         document.getElementById("outcome").innerHTML = ("You have been disproved!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[3]) {
         document.getElementById("outcome").innerHTML = ("You have been poisoned by the lizard!(Lose)");
+        scoreCounterComputer += 1;
     }
 
     if(result == option[4]) {
         document.getElementById("outcome").innerHTML = ("Draw!");
     }
 
+    return scoreCounterComputer;
+    return scoreCounterHuman;
     return result;
 }
-
-// function diceRoller1() {
-//     let rollOfDice = Math.floor((Math.random() * 6) +1);
-//     document.getElementById("total1").innerHTML = rollOfDice;
-//     return rollOfDice;
-// }
-
-
-// function diceRoller2() {
-//     let rollOfDice = Math.floor((Math.random() * 6) +1);
-//     document.getElementById("total2").innerHTML = rollOfDice;
-//     return rollOfDice;
-// }
-
-// let roll3 = 0
-// for (let i = 0; i < 1000; i++) {
-//     roll3 = diceRoller1() + diceRoller2();
-//     count[roll3] = count[roll3] + 1;
-//     document.getElementById("total3").innerHTML = count;
-// }
-
-// for (let i = 0; i < 13; i++) {
-//     let graph = document.getElementById('graph');
-//     let block = document.createElement("div");
-//     document.createElement("div");
-//     block.style.backgroundColor = "gray";
-//     block.style.width = count[i] + "px";
-//     block.style.height = "20px";
-//     block.style.marginTop = "2px";
-//     graph.appendChild(block);
-// }
